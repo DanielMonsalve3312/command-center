de7e1c79e00109b0bbc9438bb87f9b0eef65991c
Daniel Monsalve <danieldmonsalvem@gmail.com>
2024-04-11 18:54:19 -0400

    feat: not found page

--- src/components/layouts/Header/Header.tsx
+++ src/components/layouts/Header/Header.tsx
@@ -1,9 +1,11 @@
 import Image from 'next/image';
+import Link from 'next/link';
 
 export const Header = () => {
   return (
     <header className="flex w-full justify-center items-center bg-brand px-2 py-6 xs:px-4 sm:px-8">
       <div className="w-32">
+        <Link href="/">
           <Image
             src="/logo.png"
             width={135}
@@ -11,7 +13,8 @@ export const Header = () => {
             className="w-full"
             alt="Command center"
             title="Command center"
-          />
+            />
+        </Link>
       </div>
     </header>
   );
